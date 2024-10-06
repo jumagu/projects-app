2de25109feafc27ad7107fa17d04b3f8df0079d3
jumagu <jmgg.gutierrez@gmail.com>
2024-10-06 00:04:18 -0500

    feat: Add pinia Store to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
+import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import router from './router';
 import App from './App.vue';
 
 import './assets/main.css';
 
+const pinia = createPinia();
 const app = createApp(App);
 
+app.use(pinia);
 app.use(router);
 
 app.mount('#app');
