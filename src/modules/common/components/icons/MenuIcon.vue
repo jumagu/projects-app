d663566528128e37880c96fbf858ed33147a8383
jumagu <jmgg.gutierrez@gmail.com>
2024-10-06 19:19:16 -0500

    feat: add icons

---
+++ src/modules/common/components/icons/MenuIcon.vue
@@ -0,0 +1,10 @@
+<template>
+  <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M4 6h16M4 12h16M4 18h16"
+    ></path>
+  </svg>
+</template>
