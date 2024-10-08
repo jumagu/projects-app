35f0e31665467dd01329b0796717ab303f8f0d98
jumagu <jmgg.gutierrez@gmail.com>
2024-10-08 00:06:43 -0500

    feat: add BreadCrumbs component

---
+++ src/modules/common/components/ui/BreadCrumbs.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import FolderIcon from '../icons/FolderIcon.vue';
+import AddFileIcon from '../icons/AddFileIcon.vue';
+
+interface Props {
+  name: string;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="breadcrumbs text-sm">
+    <ul>
+      <li>
+        <router-link to="/projects" class="inline-flex items-center gap-2">
+          <folder-icon class="h-4 w-4" />
+          Projects
+        </router-link>
+      </li>
+
+      <li>
+        <span class="inline-flex items-center gap-2">
+          <add-file-icon class="h-4 w-4" />
+          {{ name }}
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
