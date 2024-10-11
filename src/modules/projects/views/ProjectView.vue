0dc9020a6d1be40bd11ed230ce595eaf94c8517d
jumagu <jmgg.gutierrez@gmail.com>
2024-10-11 13:06:47 -0500

    refactor: improve responsiveness

--- src/modules/projects/views/ProjectView.vue
+++ src/modules/projects/views/ProjectView.vue
@@ -3,35 +3,88 @@ import { ref, watch } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 
 import { useProjectsStore } from '../stores/projects.store';
-import type { IProject } from '../interfaces/project.interface';
 import PenIcon from '@/modules/common/components/icons/PenIcon.vue';
+import type { IProject, ITask } from '../interfaces/project.interface';
 import InputModal from '@/modules/common/components/ui/InputModal.vue';
-import BreadCrumbs from '@/modules/common/components/ui/BreadCrumbs.vue';
+import CustomModal from '@/modules/common/components/ui/CustomModal.vue';
 import ButtonCircle from '@/modules/common/components/ui/ButtonCircle.vue';
-import AddCircleIcon from '@/modules/common/components/icons/AddCircleIcon.vue';
+import AddTaskIcon from '@/modules/common/components/icons/AddTaskIcon.vue';
 import TrashCanIcon from '@/modules/common/components/icons/TrashCanIcon.vue';
 
 const route = useRoute();
 const router = useRouter();
 const projectsStore = useProjectsStore();
 
-const modalOpen = ref(false);
+const modalDeleteOpen = ref(false);
+const taskToDelete = ref<ITask>();
 const project = ref<IProject | undefined>(undefined);
+const modalState = ref<{
+  open: boolean;
+  title: string;
+  value?: string;
+  btnText: string;
+  subtitle: string;
+  inputPlaceholder: string;
+  onValue?: (value: string) => void;
+}>({
+  open: false,
+  title: '',
+  btnText: '',
+  subtitle: 'Set a descriptive name',
+  inputPlaceholder: 'Do something',
+});
 
-const addTaskHandler = (taskName: string) => {
+const addTaskHandler = () => {
   if (!project.value) return;
-  projectsStore.addTaskToProject(project.value.id, taskName);
+  modalState.value = {
+    ...modalState.value,
+    open: true,
+    title: 'Add a new task to ' + project.value.name,
+    value: '',
+    btnText: 'Add',
+    onValue: (value) => {
+      if (!project.value) return;
+      projectsStore.addTaskToProject(project.value.id, value);
+    },
+  };
+};
+
+const updateTaskHandler = (id: string, name: string) => {
+  if (!project.value) return;
+  modalState.value = {
+    ...modalState.value,
+    open: true,
+    title: `Editing task: ${name}`,
+    value: name,
+    btnText: 'Update',
+    onValue: (value) => {
+      if (!project.value) return;
+      console.log({ value });
+      projectsStore.updateProjectTaskName(project.value.id, id, value);
+    },
+  };
+};
+
+const deleteTaskHandler = (task: ITask) => {
+  taskToDelete.value = task;
+  modalDeleteOpen.value = true;
+};
+
+const confirmTaskDeletionHandler = () => {
+  if (!taskToDelete.value || !project.value) return;
+  projectsStore.deleteProjectTask(project.value.id, taskToDelete.value.id);
+  modalDeleteOpen.value = false;
 };
 
 const toggleTaskHandler = (taskId: string) => {
   if (!project.value) return;
-  projectsStore.toggleTaskCompleted(project.value?.id, taskId);
+  projectsStore.toggleTaskCompleted(project.value.id, taskId);
 };
 
 watch(
   () => [route.params.id, projectsStore.projectList],
   () => {
-    project.value = projectsStore.projectList.find((project) => project.id === route.params.id);
+    project.value = projectsStore.findProjectById(route.params.id as string);
     if (!project.value) router.replace('/');
   },
   {
@@ -41,13 +94,9 @@ watch(
 </script>
 
 <template>
-  <section class="mx-4">
-    <bread-crumbs :name="project?.name ?? ''" />
-  </section>
-
   <section>
-    <div class="overflow-x-auto">
-      <table class="table">
+    <div class="h-[calc(100vh-84px)] overflow-x-auto">
+      <table v-if="project?.tasks.length ?? 0 > 0" class="table table-pin-rows">
         <thead>
           <tr>
             <th>#</th>
@@ -75,33 +124,45 @@ watch(
               }}
             </td>
             <td>
-              <button class="btn btn-circle btn-ghost btn-sm">
-                <pen-icon class="w-5 h-5 text-accent" />
-              </button>
               <button
                 class="btn btn-circle btn-ghost btn-sm"
-                @click="projectsStore.deleteProjectTask(project!.id, task.id)"
+                @click="updateTaskHandler(task.id, task.name)"
               >
+                <pen-icon class="w-5 h-5 text-accent" />
+              </button>
+              <button class="btn btn-circle btn-ghost btn-sm" @click="deleteTaskHandler(task)">
                 <trash-can-icon class="w-5 h-5 text-error" />
               </button>
             </td>
           </tr>
         </tbody>
       </table>
+      <div v-else class="h-full flex flex-col justify-center items-center">
+        <img
+          class="w-36 brightness-75 pointer-events-none"
+          src="../../../assets/images/to-do-list.png"
+          alt="Empty Folder"
+        />
+        <p class="text-xl font-semibold my-2">No task added yet</p>
+        <p>Click the rounded button to add one</p>
+      </div>
     </div>
 
-    <input-modal
-      btn-text="Add"
-      :title="'Add a new task to ' + project?.name"
-      subtitle="Set a descriptive name"
-      input-placeholder="Do something"
-      :open="modalOpen"
-      @close="modalOpen = false"
-      @value="addTaskHandler"
-    />
+    <input-modal v-bind="modalState" @close="modalState.open = false" />
+    <custom-modal :open="modalDeleteOpen" @close="modalDeleteOpen = false">
+      <template #header>
+        <h3 class="text-lg font-bold mb-2">
+          Are you sure to delete the task '{{ taskToDelete?.name }}'?
+        </h3>
+        <p>This action cannot be undone</p>
+      </template>
+      <template #actions>
+        <button class="btn btn-error" @click="confirmTaskDeletionHandler">Confirm</button>
+      </template>
+    </custom-modal>
 
-    <button-circle class="btn-primary fixed bottom-8 right-8" @click="modalOpen = true">
-      <add-circle-icon />
+    <button-circle class="btn-primary fixed bottom-8 right-8" @click="addTaskHandler">
+      <add-task-icon class="w-10 h-10" />
     </button-circle>
   </section>
 </template>
--- src/modules/projects/views/ProjectsView.vue
+++ src/modules/projects/views/ProjectsView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 
 import { useProjectsStore } from '../stores/projects.store';
 import type { IProject } from '../interfaces/project.interface';
@@ -10,7 +11,11 @@ import ButtonCircle from '@/modules/common/components/ui/ButtonCircle.vue';
 import TrashCanIcon from '@/modules/common/components/icons/TrashCanIcon.vue';
 import AddCircleIcon from '@/modules/common/components/icons/AddCircleIcon.vue';
 
+const router = useRouter();
+const projectsStore = useProjectsStore();
+
 const modalDeleteOpen = ref(false);
+const projectToDelete = ref<IProject>();
 const modalState = ref<{
   open: boolean;
   title: string;
@@ -26,9 +31,6 @@ const modalState = ref<{
   subtitle: 'Set a descriptive name',
   inputPlaceholder: 'Name of the project',
 });
-const projectToDelete = ref<IProject>();
-
-const projectsStore = useProjectsStore();
 
 const addProjectHandler = () => {
   modalState.value = {
@@ -65,59 +67,78 @@ const confirmProjectDeletionHandler = () => {
 </script>
 
 <template>
-  <div class="overflow-x-auto">
-    <table class="table">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Project</th>
-          <th>Tasks</th>
-          <th>Progress</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(project, index) in projectsStore.projectList" :key="project.id" class="hover">
-          <th>{{ index + 1 }}</th>
-          <td>{{ project.name }}</td>
-          <td>{{ project.tasks.length }}</td>
-          <td>
-            <progress
-              max="100"
-              :value="project.completion"
-              class="progress progress-primary w-auto"
-            ></progress>
-          </td>
-          <td>
-            <button
-              class="btn btn-circle btn-ghost btn-sm"
-              @click="updateProjectHandler(project.id, project.name)"
-            >
-              <pen-icon class="w-5 h-5 text-accent" />
-            </button>
-            <button class="btn btn-circle btn-ghost btn-sm" @click="deleteProjectHandler(project)">
-              <trash-can-icon class="w-5 h-5 text-error" />
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  <section>
+    <div class="h-[calc(100vh-84px)] overflow-x-auto">
+      <table v-if="projectsStore.projectList.length > 0" class="table table-pin-rows">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Project</th>
+            <th>Tasks</th>
+            <th>Progress</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(project, index) in projectsStore.projectList"
+            :key="project.id"
+            class="hover cursor-pointer"
+            @click="router.push(`/project/${project.id}`)"
+          >
+            <th>{{ index + 1 }}</th>
+            <td>{{ project.name }}</td>
+            <td>{{ project.tasks.length }}</td>
+            <td>
+              <progress
+                max="100"
+                :value="project.completion"
+                class="progress progress-primary w-auto"
+              ></progress>
+            </td>
+            <td>
+              <button
+                class="btn btn-circle btn-ghost btn-sm"
+                @click.stop="updateProjectHandler(project.id, project.name)"
+              >
+                <pen-icon class="w-5 h-5 text-accent" />
+              </button>
+              <button
+                class="btn btn-circle btn-ghost btn-sm"
+                @click.stop="deleteProjectHandler(project)"
+              >
+                <trash-can-icon class="w-5 h-5 text-error" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div v-else class="h-full flex flex-col justify-center items-center">
+        <img
+          class="w-36 brightness-75 pointer-events-none"
+          src="../../../assets/images/empty-folder.png"
+          alt="Empty Folder"
+        />
+        <p class="text-xl font-semibold my-2">No projects created yet</p>
+        <p>Click the rounded button to create one</p>
+      </div>
+    </div>
 
-  <input-modal v-bind="modalState" @close="modalState.open = false" />
-  <custom-modal :open="modalDeleteOpen" @close="modalDeleteOpen = false">
-    <template #header>
-      <h3 class="text-lg font-bold mb-2">
-        Are you sure to delete the project '{{ projectToDelete?.name }}'?
-      </h3>
-      <p>This action cannot be undone</p>
-    </template>
-    <template #actions>
-      <button class="btn btn-error" @click="confirmProjectDeletionHandler">Confirm</button>
-    </template>
-  </custom-modal>
+    <input-modal v-bind="modalState" @close="modalState.open = false" />
+    <custom-modal :open="modalDeleteOpen" @close="modalDeleteOpen = false">
+      <template #header>
+        <h3 class="text-lg font-bold mb-2">
+          Are you sure to delete the project '{{ projectToDelete?.name }}'?
+        </h3>
+        <p>This action cannot be undone</p>
+      </template>
+      <template #actions>
+        <button class="btn btn-error" @click="confirmProjectDeletionHandler">Confirm</button>
+      </template>
+    </custom-modal>
 
-  <button-circle class="btn-primary fixed bottom-8 right-8" @click="addProjectHandler">
-    <add-circle-icon />
-  </button-circle>
+    <button-circle class="btn-primary fixed bottom-8 right-8" @click="addProjectHandler">
+      <add-circle-icon />
+    </button-circle>
+  </section>
 </template>
